f00c2876a0713aa38cc44caf30930caefd6c3e3d
PhamMinhHanh <2151053015hanh@ou.edu.vn>
2026-04-24 20:31:16 +0700

    feat: Xây dựng CSDL và giao diện Đăng nhập/Đăng ký

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mot-chuoi-bi-mat-bat-ky-cua-nhom-7'
+
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ticket_user:123456@localhost/event_ticketing'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
